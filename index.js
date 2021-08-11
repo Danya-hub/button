82f6013d9a4ccf72462fe0a7a6b4b16faad91553
Danya-hub <6694524@gmail.com>
2021-08-11 21:17:09 +0300

    create button with animation

---
+++ index.js
@@ -0,0 +1,71 @@
+'use strict';
+
+class AnimButton {
+    constructor(nameAnim, timeout, elem) {
+        this.timeout = timeout;
+        this.elem = elem;
+        this.name = nameAnim;
+        this.__init__();
+    }
+
+    __init__() {
+        this.circle = document.createElement('span');
+        this.circle.style.cssText = `
+            position: absolute;
+            background: #fff;
+            transform: translate(-50%, -50%);
+            border-radius: 50%;
+            pointer-events: none;
+        `;
+
+        this._setMove();
+        this._setAnim();
+        this._remElem();
+    }
+
+    _setMove() {
+        this.elem.target.append(this.circle);
+
+        this.x = this.elem.clientX - this.elem.target.offsetLeft,
+            this.y = this.elem.clientY - this.elem.target.offsetTop;
+        this.d = Math.max(this.elem.target.offsetWidth, this.elem.target.offsetHeight);
+
+        this.circle.style.left = `${this.x}px`,
+            this.circle.style.top = `${this.y}px`;
+    }
+
+    _setAnim() {
+        this.DOMStyle = document.createElement('style');
+        this.DOMStyle.type = 'text/css';
+        this.DOMStyle.id = `${this.name}Keyframes`;
+        document.head.append(this.DOMStyle);
+
+        this.circle.style.animation = `${this.name} ${this.timeout}s linear`;
+        this.DOMStyle.sheet.insertRule(
+            `@keyframes opacity {
+                0% {
+                    width: 0px;
+                    height: 0px;
+                    opacity: 0.6;
+                }
+                
+                100% {
+                    width: ${this.d}px;
+                    height: ${this.d}px;
+                    opacity: 0;
+                }
+            }`,
+            this.DOMStyle.sheet.cssRules.length);
+    }
+
+    _remElem() {
+        setTimeout(() => {
+            this.DOMStyle.remove();
+            this.circle.remove();
+        }, this.timeout * 1000);
+    }
+}
+
+document.querySelector('button').addEventListener('click', (e) => {
+    new AnimButton('opacity', 0.4, e);
+})
